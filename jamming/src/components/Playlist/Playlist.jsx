f5d5ad5a01ac1be4a195c6a6bbdc184379b2c7fa
Tianna <140460149+tianna-spears@users.noreply.github.com>
2024-08-23 16:44:01 +0100

    Implement Playlist Renaming

--- jamming/src/components/Playlist/Playlist.jsx
+++ jamming/src/components/Playlist/Playlist.jsx
@@ -4,29 +4,55 @@ import Tracklist from '../Tracklist/Tracklist';
 
 function Playlist ({ name, tracks, onRemove, onNameChange }) {
     const [newName, setNewName] = useState(name);
+    const [edit, setEdit] = useState(false);
+    const [showEditButton, setShowEditButton] = useState(false);
 
-    const handleNameChange= (event) => {
+    const handleNameChange = (event) => {
         setNewName(event.target.value);
-        onNameChange(event.target.value);
     };
+
+    const handleEdit = () => {
+        setEdit(true);
+        setShowEditButton(true);
+    }
+
+    const handleEditEvent = (event) => {
+        if (event.key === 'Enter') {
+            setEdit(false);
+            setShowEditButton(false);
+            onNameChange(newName);
+        }
+    }
     
     return (
     <div>
         <h2>
-            <label> Playlist Name: </label>
+        {showEditButton ? (
+            <label> Playlist Name:
             <input 
             type= "text"
             value= {newName}
             onChange= {handleNameChange}
+            onKeyDown= {handleEditEvent}
+            autoFocus
             />
+            </label>
+        ) : (
+        <span onClick= {handleEdit}>{name}</span>
+        )}
         </h2>
+        {!showEditButton && (
+            <button onClick={handleEdit}> Edit Playlist Name</button>
+        )
+
+        }
 
         <Tracklist 
         tracks={tracks}
         onRemove= {onRemove}
         />
     </div>
-    )
-}
+    
+    )}
 
 export default Playlist;
